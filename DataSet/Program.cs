536aa9fe81876095d3b75b0fafd1b12ca54fd7d1
Александр <xplayeradv@gmail.com>
2020-06-22 15:29:56 +0400

    2 задание из практики к 4 лекции

---
+++ DataSet/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace DataSet
+{
+    using System;
+    using System.Data;
+    using System.Text;
+
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args">Aruguments of program</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Converts DataSet into string
+        /// </summary>
+        /// <param name="data">DataSet tables</param>
+        /// <param name="recordDelimiter">Delimeter for records</param>
+        /// <param name="columnDelimiter">Delimiter for header</param>
+        /// <returns>Returns all data in one string</returns>
+        public string Method(DataSet data, char recordDelimiter, char columnDelimiter)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (DataTable dt in data.Tables)
+            {
+                result.Append(dt.TableName + "\n");
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    result.Append(columnDelimiter + column.ColumnName);
+                }
+
+                result.Append('\n');
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    var cells = row.ItemArray;
+                    foreach (object cell in cells)
+                    {
+                        result.Append(recordDelimiter + cell.ToString());
+                    }
+
+                    result.Append('\n');
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
