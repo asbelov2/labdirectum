fd17712242e06a0b28cf195d8467265c6bd1c139
Александр <xplayeradv@gmail.com>
2020-06-23 21:55:25 +0400

    1 задание из 5 лекции

---
+++ StringEqual/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringEqual
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args">Aruguments of program</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new StringValue("AAA").Equals(new StringValue("AAA")));
+        }
+    }
+}
---
+++ StringEqual/StringValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringEqual
+{
+    public class StringValue
+    {
+        public string Value { get; private set; }
+        public StringValue(string value)
+        {
+            this.Value = value;
+        }
+        public override bool Equals(object str)
+        {
+            return (str.GetHashCode()==this.GetHashCode());
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Value);
+        }
+    }
+}
+
