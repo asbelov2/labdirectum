386926db3686bd0549f96781631b9b6de0fb78fe
Лихвар Дмитрий (Likhvar_DS) <Likhvar_DS@directum.ru>
2020-06-24 11:15:55 +0400

    Замечания к 5 заданию.

--- ComplexNumber/Complex.cs
+++ ComplexNumber/Complex.cs
@@ -17,7 +17,7 @@
         /// <summary>
         /// Gets or sets real part
         /// </summary>
-        public double Re { get; set; }
+        public double Re { get; set; }  // Лучше сделать неизменяемым. Убрать сет. Задавать через конструктор.
 
         /// <summary>
         /// Gets or sets imagine part
@@ -31,7 +31,7 @@
         /// <returns>Positive if this object more than other, Negative if less, 0 if they are equal</returns>
         public int CompareTo(Complex other)
         {
-            return Math.Sign(Math.Sqrt((this.Re * this.Re) + (this.Im * this.Im)) - Math.Sqrt((other.Re * other.Re) + (other.Im * other.Im)));
+            return Math.Sign(Math.Sqrt((this.Re * this.Re) + (this.Im * this.Im)) - Math.Sqrt((other.Re * other.Re) + (other.Im * other.Im)));  // Лучше сделать свойство с вычислением абсолютного значения.
         }
 
         /// <summary>
@@ -41,12 +41,13 @@
         /// <returns>Positive if this object more than other, Negative if less, 0 if they are equal</returns>
         public int CompareTo(object obj)
         {
-            if (this.GetType() != obj.GetType())
+            if (this.GetType() != obj.GetType())  // Более правильная проверка: obj is Complex
             {
                 throw new ArgumentException("Not complex number");
             }
 
-            return this.CompareTo((Complex)obj);
+            return this.CompareTo((Complex)obj);  // Либо можно даже obj is Complex value. И value передавать в CompareTo в if конструкции.
+                                                  // А исключение бросать после if'а.
         }
     }
 }
