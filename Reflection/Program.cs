7845cbf51563b7ef4a05f37ed80de89efe023ff5
Александр <xplayeradv@gmail.com>
2020-07-01 18:15:17 +0400

    9 практика 1 задание

---
+++ Reflection/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Reflection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    /// <summary>
+    /// Program class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        /// <param name="args">Aruguments of program</param>
+        public static void Main(string[] args)
+        {
+            Test test = new Test("a", "b", "c");
+            var props = GetProperties(test);
+            foreach (var property in props)
+            {
+                Console.WriteLine(property);
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// This method gets names of all read-write properties
+        /// </summary>
+        /// <param name="obj">Target object</param>
+        /// <returns>Names of properties</returns>
+        public static List<string> GetProperties(object obj)
+        {
+            Type type = obj.GetType();
+            PropertyInfo[] properties = type.GetProperties();
+            List<string> result = new List<string>();
+            foreach (var property in properties)
+            {
+                if (property.CanRead && property.CanWrite)
+                {
+                    result.Add(property.Name);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Reflection/Test.cs
@@ -0,0 +1,41 @@
+﻿namespace Reflection
+{
+    /// <summary>
+    /// Test class
+    /// </summary>
+    public class Test
+    {
+        /// <summary>
+        /// Initializes a new instance of the Test class
+        /// </summary>
+        /// <param name="a">String a</param>
+        /// <param name="b">String b</param>
+        /// <param name="c">String c</param>
+        public Test(string a, string b, string c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        /// <summary>
+        /// Gets or sets A
+        /// </summary>
+        public string A { get; set; }
+
+        /// <summary>
+        /// Sets B
+        /// </summary>
+        public string B
+        {
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets C
+        /// </summary>
+        public string C { get; }
+    }
+}
