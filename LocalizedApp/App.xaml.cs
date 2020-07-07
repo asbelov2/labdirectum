1da9b6ffe7f99d8f88b17abb5844d6b5ca19b680
Александр <xplayeradv@gmail.com>
2020-07-07 09:46:16 +0400

    12 практика

---
+++ LocalizedApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace LocalizedApp
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes new instance of app
+        /// </summary>
+        static App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ru");
+        }
+    }
+}
---
+++ LocalizedApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace LocalizedApp
+{
+    using System;
+    using System.Reflection;
+    using System.Resources;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes new instance of MainWindow
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method for button click event
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event parameters</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(TextMessages.HelloMessage);
+        }
+
+        /// <summary>
+        /// Method for initialization of window event
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event parameters</param>
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            var resMan = new ResourceManager("LocalizedApp.TextMessages", Assembly.GetExecutingAssembly());
+            MessageBox.Show(resMan.GetString("HelloMessage", new System.Globalization.CultureInfo("en")));
+        }
+    }
+}
