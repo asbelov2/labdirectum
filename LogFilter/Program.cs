dc37d53df03dfae2e697a91a96e4c5d7ff6a9253
Лихвар Дмитрий (Likhvar_DS) <Likhvar_DS@directum.ru>
2020-07-07 10:41:31 +0400

    Замечания к 11.

--- LogFilter/Program.cs
+++ LogFilter/Program.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.IO;
+    using System.IO;    // Не используемые using'и.
     using System.Linq;
 
     /// <summary>
@@ -29,11 +29,12 @@
         /// </summary>
         /// <param name="path">Path to log</param>
         /// <returns>sorted by date log</returns>
-        public static List<string> GetSortedByDateLog(string path)
+        public static List<string> GetSortedByDateLog(string path)  // Не хватает фильтрации за указанную дату.
         {
             var text = new Text(path);
-            DateTime date = new DateTime();
-            var sortedLog = text.Where(t => DateTime.TryParseExact(t.Substring(0, "dd.MM.yyyy hh:mm:ss".Length), "dd.MM.yyyy hh:mm:ss", null, DateTimeStyles.None, out date))
+            DateTime date = new DateTime(); // Зачем инициализировать date?
+            var sortedLog = text            // "dd.MM.yyyy hh:mm:ss" можно вынести в константу.
+                .Where(t => DateTime.TryParseExact(t.Substring(0, "dd.MM.yyyy hh:mm:ss".Length), "dd.MM.yyyy hh:mm:ss", null, DateTimeStyles.None, out date))
                 .OrderBy(t => DateTime.Parse(t.Substring(0, "dd.MM.yyyy hh:mm:ss".Length)));
             return sortedLog.ToList();
         }
