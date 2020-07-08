74a212493335044987d9de90ad8e06fed74c6e92
Александр <xplayeradv@gmail.com>
2020-07-08 18:08:05 +0400

    Исправил 11

--- LogFilter/Program.cs
+++ LogFilter/Program.cs
@@ -16,7 +16,7 @@
         /// <param name="args">Aruguments of program</param>
         public static void Main(string[] args)
         {
-            var sortedTxt = GetSortedByDateLog("Log.txt", "10.05.1965");
+            var sortedTxt = GetSortedByDateLog("Log.txt", DateTime.Parse("10.05.1965"));
             foreach (var str in sortedTxt)
             {
                 Console.WriteLine(str);
@@ -28,14 +28,14 @@
         /// </summary>
         /// <param name="path">Path to log</param>
         /// <returns>sorted by date log</returns>
-        public static List<string> GetSortedByDateLog(string path, string date)  // Почему дата передаётся строкой?
+        public static List<string> GetSortedByDateLog(string path, DateTime date)
         {
             const string datePattern = "dd.MM.yyyy hh:mm:ss";
             var text = new Text(path);
             var sortedLog = text
                 .Where(t => DateTime.TryParseExact(t.Substring(0, datePattern.Length), datePattern, null, DateTimeStyles.None, out var date))
                 .OrderBy(t => DateTime.Parse(t.Substring(0, datePattern.Length)))
-                .Where(t => DateTime.Parse(t.Substring(0, datePattern.Length)).ToShortDateString() == date);
+                .Where(t => DateTime.Parse(t.Substring(0, datePattern.Length)).Date == date);
             return sortedLog.ToList();
         }
     }
